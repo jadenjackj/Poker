323a0a70ca42eb3fb05afb27addb27340d334d5e
Hanson Liu <hansonliu42@gmail.com>
2022-12-11 18:26:01 -0500

    Finished the basic structure to evaluate hand type

--- Hand.py
+++ Hand.py
@@ -4,7 +4,7 @@ class Hand:
     def __init__(self, hand) -> int:
 
         #the status of your hand
-        self.high_card = False
+        self.high_card = 0 #note: I'm changing high_card to an int because we need to know what's the high card anyways
         self.pocket_pair = False
         self.one_pair = False
         self.two_pair = False
@@ -35,40 +35,96 @@ class Hand:
         return duplicates
 
 
-    #todo:
     #converts the hand into a hand I can evaluate numbers wise. converts the jack to 11, queen to 12, etc.
-    def convert(hand):
+    def convert_hand(hand, low_or_high):
         converted_hand = []
         for i in hand:
             if(i == 'J'):
                 converted_hand.append(11)
+            elif(i == 'Q'):
+                converted_hand.append(12)
+            elif(i == 'K'):
+                converted_hand.append(13)
+            elif(i == 'A' and low_or_high == 'high'):
+                converted_hand.append(14)
+            elif(i == 'A' and low_or_high == 'low'):
+                converted_hand.append(1)
+            else:
+                converted_hand.append(int(i))
+        return converted_hand
 
 #----------------------------------------------------------------------------------------------------
 
     #note: all the functions below determines if a hand has a particular strength
     def quads(self):
-        duplicates = duplicates(self.hand)
+        duplicate_list = self.duplicates(self.hand)
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         for i in ranks:
-            if(duplicates.count(i) == 6):
+            if(duplicate_list.count(i) == 6):
                 self.quads=True
 
 
     def trips(self):
-        duplicates = duplicates(self.hand)
+        duplicate_list = self.duplicates(self,self.hand)
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         for i in ranks:
-            if(duplicates.count(i) == 3):
+            if(duplicate_list.count(i) == 3):
                 self.trips=True
     
     #evaluates both one_pair and two_pair
     def pairs(self):
-        duplicates = duplicates(self.hand)
+        duplicate_list = self.duplicates(self.hand)
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         for i in ranks:
-            if(duplicates.count(i) == 2 and self.one_pair == True):
+            if(duplicate_list.count(i) == 2 and self.one_pair == True):
                 self.two_pair=True
                 self.one_pair=False #doing this because if we have a two pair then that means we don't simply have just a one pair anymore
-            if(duplicates.count(i) == 2):
+            if(duplicate_list.count(i) == 2):
                 self.one_pair=True
 
+    def straight(self):
+        #2 possible converted hands because Ace an be either 1 strength or 14 strength
+        low_converted_hand = self.convert_hand(self.hand, 'low')
+        high_converted_hand = self.convert_hand(self.hand, 'high')
+        low_converted_hand.sort() #sorts the converted hand from smallest to largest
+        high_converted_hand.sort()
+
+        low_straight = True
+        high_straight = True
+        #if one of the cards is not one less than the next than it ain't a straight
+        #checks to see if we have a straight with either A = 1 or A = 14
+        for i in range(1,len(low_converted_hand)):
+            if(low_converted_hand[i-1]+1 != low_converted_hand[i]):
+                low_straight = False
+        for i in range(1,len(high_converted_hand)):
+            if(high_converted_hand[i-1]+1 != high_converted_hand[i]):
+                high_straight = False
+
+        if(low_straight or high_straight):
+            self.straight = True
+
+    def flush(self):
+        suit = self.hand[0][1] #gets the suit that the flush is supposed to follow
+        self.flush = True
+        for i in self.hand: #if one of the cards doesn't have the suit then it ain't a flush
+            if(i[1] != suit):
+                self.flush = False
+
+    def high_card(self):
+        converted_hand = self.convert_hand(self.hand, 'high')
+        converted_hand.sort()
+        return converted_hand[-1] #returns the highest card value
+
+    def straight_flush(self):
+        if(self.flush and self.straight):
+            self.straight_flush = True
+
+    def full_house(self):
+        if(self.trips and self.one_pair):
+            self.fullhouse = True
+
+    def royal_flush(self):
+        if(self.straight_flush):
+            converted_hand = self.convert_hand(self.hand, 'high')
+            if(converted_hand[0] == 10 and converted_hand[-1] == 14):
+                self.royal_flush = True
